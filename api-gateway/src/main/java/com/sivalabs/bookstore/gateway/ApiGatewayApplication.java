b581aef6590c8a022051c2f7e916b8e4911a0083
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2024-04-21 09:46:29 +0530

    Implementing api-gateway

---
+++ api-gateway/src/main/java/com/sivalabs/bookstore/gateway/ApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.sivalabs.bookstore.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
