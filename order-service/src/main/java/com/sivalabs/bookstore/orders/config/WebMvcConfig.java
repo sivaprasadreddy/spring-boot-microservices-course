87b011c4087cf1ce82cc29228eca80f6d703b180
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2024-04-21 16:41:37 +0530

    Implementing API Gateway

--- order-service/src/main/java/com/sivalabs/bookstore/orders/config/WebMvcConfig.java
+++ order-service/src/main/java/com/sivalabs/bookstore/orders/config/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package com.sivalabs.bookstore.catalog.config;
+package com.sivalabs.bookstore.orders.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
