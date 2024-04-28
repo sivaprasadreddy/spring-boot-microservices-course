141ed6c243efd4bd38d2fab3ac678ee0ae9db05c
Anton Ždanov <anton@azdanov.dev>
2024-04-28 04:25:27 +0300

    Add validated annotation (#4)
    For min annotation to work the validated annotation is needed

--- catalog-service/src/main/java/com/sivalabs/bookstore/catalog/ApplicationProperties.java
+++ catalog-service/src/main/java/com/sivalabs/bookstore/catalog/ApplicationProperties.java
@@ -3,6 +3,8 @@ package com.sivalabs.bookstore.catalog;
 import jakarta.validation.constraints.Min;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 @ConfigurationProperties(prefix = "catalog")
 public record ApplicationProperties(@DefaultValue("10") @Min(1) int pageSize) {}
