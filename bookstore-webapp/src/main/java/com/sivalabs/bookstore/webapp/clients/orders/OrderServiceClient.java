24cdc1bde39f6311688cf636109a31c601ec9b60
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2024-05-03 13:30:31 +0530

    Fix code formatting

--- bookstore-webapp/src/main/java/com/sivalabs/bookstore/webapp/clients/orders/OrderServiceClient.java
+++ bookstore-webapp/src/main/java/com/sivalabs/bookstore/webapp/clients/orders/OrderServiceClient.java
@@ -5,13 +5,11 @@ import java.util.Map;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.PostExchange;
 
 public interface OrderServiceClient {
-  
+
     @PostExchange("/orders/api/orders")
     OrderConfirmationDTO createOrder(
             @RequestHeader Map<String, ?> headers, @RequestBody CreateOrderRequest orderRequest);
@@ -21,5 +19,4 @@ public interface OrderServiceClient {
 
     @GetExchange("/orders/api/orders/{orderNumber}")
     OrderDTO getOrder(@RequestHeader Map<String, ?> headers, @PathVariable String orderNumber);
-
 }
